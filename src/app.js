a40f715bfa42313d219b3f85cf480b622fffd74f
CountNick <meijer.nick1@gmail.com>
2021-11-09 12:13:44 +0100

    feat: render cards in grid

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import './scss/app.scss'
 import  { API } from './helpers/api.js'
+import { Card } from './templates/card.js'
+
 
 const app = async () => {
+    // get pictures from the pexels api
     const pictures = await API.getPictures()
-
-    console.log(pictures)
+    console.log(pictures.photos)
+    // render card for each picture and append to body
+    const pictureGrid = document.querySelector('.card-grid__container')
+    pictures.photos.forEach(picture => {
+       pictureGrid.appendChild(Card(picture.src.medium, `Picture by ${picture.photographer}`, picture.photographer))
+    })
 }
 
 // init 
