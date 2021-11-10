a314f0db7a25c39bfea24ef6781e788220388005
CountNick <meijer.nick1@gmail.com>
2021-11-10 17:36:28 +0100

    style: header styling

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ import './scss/app.scss'
 import  { API } from './helpers/api.js'
 import { Card } from './templates/Card.js'
 import { Modal } from './templates/Modal.js'
+import { Header } from './templates/Header.js'
 
 
 const app = async () => {
     // get pictures from the pexels api
     const pictures = await API.getPictures()
+    // selects the main element
+    const main = document.querySelector('.main__container')
+    // selects the loading element
     const loader = document.querySelector('.loader')
-    console.log(pictures.photos)
     // select the grid container
     const pictureGrid = document.querySelector('.card-grid__container')
+    // select the first picture
+    const headerImg = [pictures.photos[0].src.medium, pictures.photos[0].src.large, pictures.photos[0].src.large2x]
+    // render the header
+    main.insertBefore(Header(
+        'The art of skateboarding',
+        'A responsive gallery about my passion skateboarding made with vanilla js and parcel as build tooling.',
+        headerImg
+    ), pictureGrid)
+    console.log(pictures.photos)
     // loop through pictures array
     pictures.photos.forEach(picture => {
         // select sources to be used in template
--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,13 +1,18 @@
-export const Header = (title, src, alt) => {
+export const Header = (title, subtitle, src, alt) => {
     // transform string to dom node
     const template = document.createRange().createContextualFragment(`
-    <header class="modal__container">
+    <header class="header__container">
         <div class="header__content">
-            <h1 class="header__title">${title}</h1>
-            <p class="header__subtitle">${subtitle}</p>
+            <div class="header__title-container">
+                <h1 class="header__title">${title}</h1>
+                <p class="header__subtitle">${subtitle}</p>
+            </div>
             <img class="header__image"
                 loading=eager
                 src="${src}"
+                srcset="${src[0]} 480w,
+                    ${src[1]} 750w,
+                    ${src[2]} 1000w"
                 alt="${alt}"
             >
         </div>
