2300a7057f87b37b27c5a63dfb93a6fd0ba06aa4
CountNick <meijer.nick1@gmail.com>
2021-11-10 16:20:32 +0100

    feat: add click event on cards

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import './scss/app.scss'
 import  { API } from './helpers/api.js'
-import { Card } from './templates/card.js'
+import { Card } from './templates/Card.js'
+import { Modal } from './templates/Modal.js'
 
 
 const app = async () => {
@@ -15,10 +16,18 @@ const app = async () => {
         // select sources to be used in template
         const picturesSources = [picture.src.medium, picture.src.large, picture.src.large2x]
         // render a card template for each picture and append it to the grid
-        pictureGrid.appendChild(Card(picturesSources, `Picture by ${picture.photographer}`, picture.photographer))
+        pictureGrid.appendChild(Card(picturesSources, `Picture by ${picture.photographer}`, picture.photographer, picture.id))
     })
     // remove the loader when cards are rendered
     loader.remove()
+    // select all cards
+    const allCards = document.querySelectorAll('.card__container')
+    // add click event to each card
+    allCards.forEach(card => card.addEventListener('click', (e) => {
+        // find the clicked picture in pictures array based on id
+        const clickedPictue = pictures.photos.find(picture => e.target.id == picture.id)
+        console.log('clickedPicture: ', clickedPictue)
+    }))
 }
 
 // init 
