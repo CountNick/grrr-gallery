04a5ef3e9eae8f40cfdaa3cc23ce768077367f12
CountNick <meijer.nick1@gmail.com>
2021-11-11 12:23:00 +0100

    style: hover overlay

--- src/templates/Card.js
+++ src/templates/Card.js
@@ -2,13 +2,15 @@ export const Card = (src, alt, photographer, id) => {
     // transform string to dom node
     const template = document.createRange().createContextualFragment(`
     <section class="card__container" id="${id}">
-        <img class="card__image" id="${id}"
-            src="${src[0]}"
-            loading=lazy
-            srcset="${src[0]} 480w,
-                    ${src[1]} 750w,
-                    ${src[2]} 1000w"
-            alt="${alt}">
+        <div class="card__image-overlay" id="${id}">
+            <img class="card__image" id="${id}"
+                src="${src[0]}"
+                loading=lazy
+                srcset="${src[0]} 480w,
+                        ${src[1]} 750w,
+                        ${src[2]} 1000w"
+                alt="${alt}">
+        </div>
         <p class="card__caption" id="${id}">This photo was made by ${photographer}</p>
         <a class="card__download-btn id=${id}" href="${src[2]}" download>Download</a>
     </section>
